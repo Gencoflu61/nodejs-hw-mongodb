9c7a66d7db55cd5dd439599b9b426bb97acb89f0
Fatih Genç <fatihgenc_61@outlook.com>
2025-12-13 20:47:21 +0300

    commit4_ödev

--- src/services/contact.js
+++ src/services/contact.js
@@ -1,7 +1,52 @@
 import Contact from "../db/models/Contact.js";
 
-export const getAllContacts = async () => {
-  return await Contact.find();
+export const getAllContacts = async (query) => {
+  const {
+    page = 1,
+    perPage = 10,
+    sortBy = 'name',
+    sortOrder = 'asc',
+    type,
+    isFavourite,
+  } = query;
+
+  const pageNumber = parseInt(page, 10);
+  const perPageNumber = parseInt(perPage, 10);
+  const skip = (pageNumber - 1) * perPageNumber;
+
+  // Build filter
+  const filter = {};
+  if (type) {
+    filter.contactType = type;
+  }
+  if (isFavourite !== undefined) {
+    filter.isFavourite = isFavourite === 'true';
+  }
+
+  // Build sort
+  const sort = {};
+  sort[sortBy] = sortOrder === 'desc' ? -1 : 1;
+
+  // Get total count
+  const totalItems = await Contact.countDocuments(filter);
+
+  // Get contacts
+  const contacts = await Contact.find(filter)
+    .sort(sort)
+    .skip(skip)
+    .limit(perPageNumber);
+
+  const totalPages = Math.ceil(totalItems / perPageNumber);
+
+  return {
+    data: contacts,
+    page: pageNumber,
+    perPage: perPageNumber,
+    totalItems,
+    totalPages,
+    hasPreviousPage: pageNumber > 1,
+    hasNextPage: pageNumber < totalPages,
+  };
 };
 
 export const getContactById = async (contactId) => {
